97820bed1c52fbb40bdbfcf6b06108ce1a4e1dfe
masud-rana44 <mdmasudrana4488@gmail.com>
2023-10-18 01:50:24 +0600

    feat: Implemented product adding functionality

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import { RouterProvider } from "react-router-dom";
 
 import router from "./Router/Router";
 import "./index.css";
+import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Toaster />
     <RouterProvider router={router} />
   </React.StrictMode>
 );
--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -1,35 +1,76 @@
+import toast from "react-hot-toast";
 import { Button } from "../components/ui/Button";
 import { FormRow } from "../components/ui/FormRow";
+import axios from "axios";
 
 const AddProduct = () => {
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+
+    const imageUrl = form.imageUrl.value;
+    const name = form.name.value;
+    const brandName = form.brandName.value;
+    const type = form.type.value;
+    const price = form.price.value;
+    const description = form.description.value;
+    const rating = form.rating.value;
+
+    if (!imageUrl || !name || !brandName || !type || !price || !description)
+      return toast.error("Required field missing");
+
+    const newProduct = {
+      name,
+      imageUrl,
+      brandName,
+      type,
+      price,
+      rating,
+      description,
+    };
+
+    try {
+      await axios.post("http://localhost:8080/api/products", newProduct);
+
+      toast.success("Product successfully added");
+    } catch (error) {
+      console.log(error);
+      toast.error(
+        error?.response?.data?.message ||
+          error?.message ||
+          "Something went wrong"
+      );
+    }
+  };
+
   return (
     <div className="container mx-auto p-5">
-      <form className="space-y-3">
+      <form onSubmit={onSubmit} className="space-y-3">
         <FormRow label="Product image">
           <input
             type="text"
-            id="imageUrl"
+            name="imageUrl"
             className="border border-gray-400 dark:border-gray-500 dark:bg-gray-100 bg-white rounded-md px-3 py-2 shadow-sm"
           />
         </FormRow>
         <FormRow label="Product name">
           <input
             type="text"
-            id="name"
+            name="name"
             className="border border-gray-400 dark:border-gray-500 dark:bg-gray-100 bg-white rounded-md px-3 py-2 shadow-sm"
           />
         </FormRow>
         <FormRow label="Brand name">
           <input
             type="text"
-            id="brandName"
+            name="brandName"
             className="border border-gray-400 dark:border-gray-500 dark:bg-gray-100 bg-white rounded-md px-3 py-2 shadow-sm"
           />
         </FormRow>
         <FormRow label="Product Type">
           <select
-            type="text"
-            id="type"
+            name="type"
             className="border border-gray-400 dark:border-gray-500 dark:bg-gray-100 bg-white rounded-md px-3 py-2 shadow-sm"
           >
             <option value="phone">Phone</option>
@@ -41,14 +82,24 @@ const AddProduct = () => {
         <FormRow label="Product price">
           <input
             type="number"
-            id="price"
+            name="price"
+            min={1}
             className="border border-gray-400 dark:border-gray-500 dark:bg-gray-100 bg-white rounded-md px-3 py-2 shadow-sm"
           />
         </FormRow>
         <FormRow label="Product description">
           <textarea
             type="text"
-            id="description"
+            name="description"
+            className="border border-gray-400 dark:border-gray-500 dark:bg-gray-100 bg-white rounded-md px-3 py-2 shadow-sm"
+          />
+        </FormRow>
+        <FormRow label="Product rating">
+          <input
+            type="number"
+            name="rating"
+            min={1}
+            max={5}
             className="border border-gray-400 dark:border-gray-500 dark:bg-gray-100 bg-white rounded-md px-3 py-2 shadow-sm"
           />
         </FormRow>
